da38f64bfb9453076f49e716b1702616a12aa99b
Asaiah Logan <asaiahlogan@hotmail.com>
2023-12-07 09:57:28 -0600

    p04

---
+++ Assignments/P04/README.md
@@ -0,0 +1,29 @@
+# Program 4 - Concurrent Image Downloader
+## Requirements
+* Program should reads a list of image URLs from a file.
+* Two versions of the downloader implemented:
+    -Sequential Version: Downloads and saves each image one after the other.
+    -Concurrent Version: Downloads and saves images concurrently using goroutines.
+* Downloaded images are saved to disk with unique names - end of image url
+* Proper error handling for failed downloads.
+* Time taken by each version to complete the downloads are printed.
+* Largely witter by ChatGPT
+* **Sequential Time: 1.6829861s**
+* **Concurrent Time: 168.1735ms**
+
+|   #   | File                                                                          | Description                                                                   |
+| :---: | ----------------------------------------------------------------------------- | ----------------------------------------------------------------------------- |
+|   1   | [main.go](main.go)                                                            | [main](main.go)                                                               |
+|   2   | [Image 1](image_1701962140001104800.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=640) | [Image 1](image_1701962140001104800.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=640) |
+|   3   | [Image 2](image_17019621400577903000)                                         | [Image 2](image_17019621400577903000)                                         |
+|   4   | [Image 3](image_1701962140244218600)                                          | [Image 3](image_1701962140244218600)                                          |
+|   5   | [Image 4](image_1701962140244218600.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=640) | [Image 3](image_1701962140244218600.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=640) |
+
+* Image 1
+![Image 1](image_1701962140001104800.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=640)
+* Image 2
+![Image 2](image_17019621400577903000)
+* Image 3
+![Image 3](image_1701962140244218600)
+* Image 4
+![Image 4](image_1701962140244218600.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=640)
---
+++ Assignments/P04/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+// Sequential version of the image downloader.
+func downloadImagesSequential(urls []string) {
+	for _, url := range urls {
+		filename := generateFilename(url)
+		err := downloadImage(url, filename)
+		if err != nil {
+			fmt.Printf("Error downloading image from %s: %v\n", url, err)
+		}
+	}
+}
+
+func generateFilename(url string) string {
+	fileExt := filepath.Ext(url)
+	return fmt.Sprintf("image_%d%s", time.Now().UnixNano(), fileExt)
+}
+
+// Concurrent version of the image downloader.
+func downloadImagesConcurrent(urls []string) {
+	var wg sync.WaitGroup
+
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			filename := generateFilename(url)
+			err := downloadImage(url, filename)
+			if err != nil {
+				fmt.Printf("Error downloading image from %s: %v\n", url, err)
+			}
+		}(url)
+	}
+
+	wg.Wait()
+}
+
+func main() {
+	urls := []string{
+		"https://img.freepik.com/free-photo/medium-shot-man-wearing-vr-glasses_23-2150394443.jpg?w=900&t=st=1701962053~exp=1701962653~hmac=fd59704b81f45683cc7faa59dd43d13e0ae168291697af259e175b52a3b9dfc5",
+		"https://img.freepik.com/free-photo/tall-lighthouse-north-sea-cloudy-sky_181624-49637.jpg?w=900&t=st=1701962092~exp=1701962692~hmac=5b97690d67a655cfee49c80ab2be3bd28c34f18f38d79647e895520518790926",
+		"https://images.unsplash.com/photo-1698778573682-346d219402b5?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=640",
+		"https://unsplash.com/photos/Bs2jGUWu4f8/download?ixid=M3wxMjA3fDB8MXx0b3BpY3x8NnNNVmpUTFNrZVF8fHx8fDJ8fDE2OTg5MDc1MDh8&w=640",
+		// Add more image URLs
+	}
+
+	// Sequential download
+	start := time.Now()
+	downloadImagesSequential(urls)
+	fmt.Printf("Sequential download took: %v\n", time.Since(start))
+
+	// Concurrent download
+	start = time.Now()
+	downloadImagesConcurrent(urls)
+	fmt.Printf("Concurrent download took: %v\n", time.Since(start))
+}
+
+// Helper function to download and save a single image.
+func downloadImage(url, filename string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("HTTP request failed with status code %d", response.StatusCode)
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Downloaded %s and saved as %s\n", url, filename)
+	return nil
+}
